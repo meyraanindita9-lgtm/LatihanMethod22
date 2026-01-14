0459476acde1172d522ca8ae3342bd1eb9a055fd
meyraanindita9-lgtm <meyraanindita9@gmail.com>
2026-01-14 09:56:28 +0700

    Method 2 Hitung Diskon

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int jumlahItem;
+
+decimal hargaItem, diskon, totalBayar;
+
+static decimal hitungDiskon(int jml_item)
+{
+    if (jml_item == 5) return 50000m;
+    else if (jml_item == 10) return 75000m;
+    else return 0;
+}
+
+static decimal hitungTotal(int jml_item, decimal hrg_item,decimal diskon_item)
+{
+    return jml_item * hrg_item - diskon_item;
+}
+
+Console.Write("Masukkan Jumlah Item: ");
+jumlahItem = int.Parse(Console.ReadLine());
+
+Console.Write("Masukkan Harga Per Item: Rp ");
+hargaItem = decimal.Parse(Console.ReadLine());
+
+diskon = hitungDiskon(jumlahItem);
+
+totalBayar = hitungTotal(jumlahItem,hargaItem, diskon);
+
+Console.WriteLine($"Total yang harus dibayar adalah Rp {totalBayar}");
